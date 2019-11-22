bd8f4d31d956b7fe28b2b61c0ba63c66a4a56851
Dan Reardon <dreardon1021@gmail.com>
2019-11-21 20:18:59 -0700

    Final changes on week one

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var closeButton = document.querySelector('#close');
 var banner = document.querySelector('#welcome');
+var containerShift = document.querySelector('.JS-hackfix')
 
 closeButton.addEventListener('click', closeBanner);
 
 function closeBanner(){
   banner.style.display = 'none';
+  containerShift.style.bottom = '112px'
 };
